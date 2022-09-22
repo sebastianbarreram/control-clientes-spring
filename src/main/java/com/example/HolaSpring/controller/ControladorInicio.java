9403199629c50ec298ef91787833567746e9be96
sebastianbarreram <sbarrera.96@hotmail.com>
2022-09-22 12:00:35 -0500

    Create persona

--- src/main/java/com/example/HolaSpring/controller/ControladorInicio.java
+++ src/main/java/com/example/HolaSpring/controller/ControladorInicio.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -18,10 +19,21 @@ public class ControladorInicio {
 
     @GetMapping("/")
     public String inicio(Model model) {
-        List<Persona> personas= personaService.listarPersonas();
+        List<Persona> personas = personaService.listarPersonas();
         log.info("Ejecutando el controlador Spring MVC");
         model.addAttribute("personas", personas);
         return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona) {
+        return "modificar";
+    }
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona) {
+        personaService.guardarPersona(persona);
+        return "redirect:/";
     }
 
 }
